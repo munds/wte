4ac3e0aba9d9905b95679060ad171973e7ec844d
Mouzima Mousumi <mouzimamousumi@calvisitor-10-105-129-70.calvisitor.1918.berkeley.edu>
2019-03-11 21:04:41 -0700

    Navigation bar for UI

--- client/src/layout/Navbar.js
+++ client/src/layout/Navbar.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import {Navbar} from 
+"react-bootstrap";
 import { Link } from "react-router-dom";
 class Navbar extends Component {
   render() {
@@ -15,6 +17,7 @@ class Navbar extends Component {
             >
               Home
             </Link>
+            
           </div>
         </nav>
       </div>
