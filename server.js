8449938abcd5731c4cad46556af519e644dc0613
Zachary Ferreira <Pearlybrah@github.com>
2019-03-13 19:33:34 -0700

    heroku build6

--- server.js
+++ server.js
@@ -17,6 +17,10 @@ app.use(bodyParser.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
 // DB Config
 key = key.mongoURI;
 const db = process.env.mongoURI || "mongodb://localhost:27017/users";
