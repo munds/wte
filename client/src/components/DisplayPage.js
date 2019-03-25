2bc4ed642d293bc29c4289243880195058afcfeb
EdgarM510 <EdgarM510@gmail.com>
2019-03-25 08:19:05 -0700

    added multi select dropdown for categories. removed proxy from yelp api call to test heroku compatibility. fixed 2 bugs that crashed the app after producing a results array shorter than the previous search

--- client/src/components/DisplayPage.js
+++ client/src/components/DisplayPage.js
@@ -1,30 +1,221 @@
 import React, { Component } from "react";
 import Slot from "react-slot-machine";
+import Select from "react-select";
 import Details from "./Details";
 import API from "../utils/API";
 import "./style.css";
+
+const options = [
+	{ value: 'acaibowls', label: 'Acai Bowls' },
+	{ value: 'afghani', label: 'Afghan' },
+	{ value: 'african', label: 'African' },
+	{ value: 'newamerican', label: 'American (New)' },
+	{ value: 'tradamerican', label: 'American (Traditional)' },
+	{ value: 'arabian', label: 'Arabian' },
+	{ value: 'argentine', label: 'Argentine' },
+	{ value: 'armenian', label: 'Armenian' },
+	{ value: 'asianfusion', label: 'Asian Fusion' },
+	{ value: 'australian', label: 'Australian' },
+	{ value: 'austrian', label: 'Austrian' },
+	{ value: 'bagels', label: 'Bagels' },
+	{ value: 'bakeries', label: 'Bakeries' },
+	{ value: 'bangladeshi', label: 'Bangladeshi' },
+	{ value: 'bbq', label: 'Barbeque' },
+	{ value: 'basque', label: 'Basque' },
+	{ value: 'beer_and_wine', label: 'Beer, Wine & Spirits' },
+	{ value: 'belgian', label: 'Belgian' },
+	{ value: 'brasseries', label: 'Brasseries' },
+	{ value: 'brazilian', label: 'Brazilian' },
+	{ value: 'breakfast_brunch', label: 'Breakfast & Brunch' },
+	{ value: 'breweries', label: 'Breweries' },
+	{ value: 'brewpubs', label: 'Brewpubs' },
+	{ value: 'british', label: 'British' },
+	{ value: 'bubbletea', label: 'Bubble Tea' },
+	{ value: 'buffets', label: 'Buffets' },
+	{ value: 'bulgarian', label: 'Bulgarian' },
+	{ value: 'burgers', label: 'Burgers' },
+	{ value: 'burmese', label: 'Burmese' },
+	{ value: 'cafes', label: 'Cafes' },
+	{ value: 'cafeteria', label: 'Cafeteria' },
+	{ value: 'cajun', label: 'Cajun & Creole' },
+	{ value: 'calabrian', label: 'Calabrian' },
+	{ value: 'cambodian', label: 'Cambodian' },
+	{ value: 'cantonese', label: 'Cantonese' },
+	{ value: 'caribbean', label: 'Caribbean' },
+	{ value: 'catalan', label: 'Catalan' },
+	{ value: 'cheesesteaks', label: 'Cheesesteaks' },
+	{ value: 'chickenshop', label: 'Chicken Shop' },
+	{ value: 'chicken_wings', label: 'Chicken Wings' },
+	{ value: 'chinese', label: 'Chinese' },
+	{ value: 'cideries', label: 'Cideries' },
+	{ value: 'coffee', label: 'Coffee & Tea' },
+	{ value: 'colombian', label: 'Colombian' },
+	{ value: 'comfortfood', label: 'Comfort Food' },
+	{ value: 'conveyorsushi', label: 'Conveyor Belt Sushi' },
+	{ value: 'creperies', label: 'Creperies' },
+	{ value: 'cuban', label: 'Cuban' },
+	{ value: 'czech', label: 'Czech' },
+	{ value: 'delis', label: 'Delis' },
+	{ value: 'diners', label: 'Diners' },
+	{ value: 'dinnertheater', label: 'Dinner Theater' },
+	{ value: 'desserts', label: 'Desserts' },
+	{ value: 'dimsum', label: 'Dim Sum' },
+	{ value: 'donuts', label: 'Donuts' },
+	{ value: 'dominican', label: 'Dominican' },
+	{ value: 'egyptian', label: 'Egyptian' },
+	{ value: 'eritrean', label: 'Eritrean' },
+	{ value: 'ethiopian', label: 'Ethiopian' },
+	{ value: 'falafel', label: 'Falafel' },
+	{ value: 'hotdogs', label: 'Fast Food' },
+	{ value: 'filipino', label: 'Filipino' },
+	{ value: 'fishnchips', label: 'Fish & Chips' },
+	{ value: 'fondue', label: 'Fondue' },
+	{ value: 'food_court', label: 'Food Court' },
+	{ value: 'foodstands', label: 'Food Stands' },
+	{ value: 'foodtrucks', label: 'Food Trucks' },
+	{ value: 'french', label: 'French' },
+	{ value: 'gamemeat', label: 'Game Meat' },
+	{ value: 'gastropubs', label: 'Gastropubs' },
+	{ value: 'gelato', label: 'Gelato' },
+	{ value: 'georgian', label: 'Georgian' },
+	{ value: 'german', label: 'German' },
+	{ value: 'gluten_free', label: 'Gluten-Free' },
+	{ value: 'greek', label: 'Greek' },
+	{ value: 'guamanian', label: 'Guamanian' },
+	{ value: 'hainan', label: 'Hainan' },
+	{ value: 'haitian', label: 'Haitian' },
+	{ value: 'halal', label: 'Halal' },
+	{ value: 'hawaiian', label: 'Hawaiian' },
+	{ value: 'himalayan', label: 'Himalayan & Nepalese' },
+	{ value: 'honduran', label: 'Honduran' },
+	{ value: 'hkcafe', label: 'Hong Kong Style Cafe' },
+	{ value: 'hotdog', label: 'Hot Dogs' },
+	{ value: 'hotpot', label: 'Hot Pot' },
+	{ value: 'hungarian', label: 'Hungarian' },
+	{ value: 'iberian', label: 'Iberian' },
+	{ value: 'icecream', label: 'Ice Cream & Frozen Yogurt' },
+	{ value: 'indpak', label: 'Indian' },
+	{ value: 'indonesian', label: 'Indonesian' },
+	{ value: 'irish', label: 'Irish' },
+	{ value: 'italian', label: 'Italian' },
+	{ value: 'izakaya', label: 'Izakaya' },
+	{ value: 'japanese', label: 'Japanese' },
+	{ value: 'juicebars', label: 'Juice Bars & Smoothies' },
+	{ value: 'kebab', label: 'Kebab' },
+	{ value: 'korean', label: 'Korean' },
+	{ value: 'kosher', label: 'Kosher' },
+	{ value: 'laotian', label: 'Laotian' },
+	{ value: 'latin', label: 'Latin American' },
+	{ value: 'lebanese', label: 'Lebanese' },
+	{ value: 'malaysian', label: 'Malaysian' },
+	{ value: 'mauritius', label: 'Mauritius' },
+	{ value: 'mediterranean', label: 'Mediterranean' },
+	{ value: 'mexican', label: 'Mexican' },
+	{ value: 'mideastern', label: 'Middle Eastern' },
+	{ value: 'modern_european', label: 'Modern European' },
+	{ value: 'mongolian', label: 'Mongolian' },
+	{ value: 'moroccan', label: 'Moroccan' },
+	{ value: 'newmexican', label: 'New Mexican' },
+	{ value: 'nicaraguan', label: 'Nicaraguan' },
+	{ value: 'noodles', label: 'Noodles' },
+	{ value: 'pakistani', label: 'Pakistani' },
+	{ value: 'panasian', label: 'Pan Asian' },
+	{ value: 'pancakes', label: 'Pancakes' },
+	{ value: 'persian', label: 'Persian & Iranian' },
+	{ value: 'peruvian', label: 'Peruvian' },
+  { value: 'pizza', label: 'Pizza' },
+	{ value: 'poke', label: 'Poke' },
+	{ value: 'polish', label: 'Polish' },
+	{ value: 'polynesian', label: 'Polynesian' },
+	{ value: 'popuprestaurants', label: 'Pop-Up Restaurants' },
+	{ value: 'portuguese', label: 'Portuguese' },
+	{ value: 'poutineries', label: 'Poutineries' },
+	{ value: 'pretzels', label: 'Pretzels' },
+	{ value: 'puertorican', label: 'Puerto Rican' },
+	{ value: 'ramen', label: 'Ramen' },
+	{ value: 'raw_food', label: 'Raw Food' },
+	{ value: 'reunion', label: 'Reunion' },
+	{ value: 'russian', label: 'Russian' },
+	{ value: 'salad', label: 'Salad' },
+	{ value: 'salvadoran', label: 'Salvadoran' },
+	{ value: 'sandwiches', label: 'Sandwiches' },
+	{ value: 'sardinian', label: 'Sardinian' },
+	{ value: 'scandinavian', label: 'Scandinavian' },
+	{ value: 'scottish', label: 'Scottish' },
+	{ value: 'seafood', label: 'Seafood' },
+	{ value: 'senegalese', label: 'Senegalese' },
+	{ value: 'shanghainese', label: 'Shanghainese' },
+	{ value: 'sicilian', label: 'Sicilian' },
+	{ value: 'singaporean', label: 'Singaporean' },
+	{ value: 'slovakian', label: 'Slovakian' },
+	{ value: 'smokehouse', label: 'Smokehouse' },
+	{ value: 'somali', label: 'Somali' },
+	{ value: 'soulfood', label: 'Soul Food' },
+	{ value: 'soup', label: 'Soup' },
+	{ value: 'southafrican', label: 'South African' },
+	{ value: 'southern', label: 'Southern' },
+	{ value: 'spanish', label: 'Spanish' },
+	{ value: 'srilankan', label: 'Sri Lankan' },
+	{ value: 'steak', label: 'Steakhouses' },
+	{ value: 'streetvendors', label: 'Street Vendors' },
+	{ value: 'supperclubs', label: 'Supper Clubs' },
+	{ value: 'sushi', label: 'Sushi' },
+	{ value: 'syrian', label: 'Syrian' },
+	{ value: 'szechuan', label: 'Szechuan' },
+	{ value: 'tacos', label: 'Tacos' },
+	{ value: 'taiwanese', label: 'Taiwanese' },
+	{ value: 'tapas', label: 'Tapas Bars' },
+	{ value: 'tea', label: 'Tea Rooms' },
+	{ value: 'teppanyaki', label: 'Teppanyaki' },
+	{ value: 'tex-mex', label: 'Tex-Mex' },
+	{ value: 'thai', label: 'Thai' },
+	{ value: 'themedcafes', label: 'Themed Cafes' },
+	{ value: 'trinidadian', label: 'Trinidadian' },
+	{ value: 'turkish', label: 'Turkish' },
+	{ value: 'tuscan', label: 'Tuscan' },
+	{ value: 'ukrainian', label: 'Ukrainian' },
+	{ value: 'uzbek', label: 'Uzbek' },
+	{ value: 'vegan', label: 'Vegan' },
+	{ value: 'vegetarian', label: 'Vegetarian' },
+	{ value: 'venezuelan', label: 'Venezuelan' },
+	{ value: 'vietnamese', label: 'Vietnamese' },
+	{ value: 'waffles', label: 'Waffles' },
+	{ value: 'wineries', label: 'Wineries' },
+	{ value: 'winetastingroom', label: 'Wine Tasting Room' },
+	{ value: 'wraps', label: 'Wraps' }
+];
 
 class YelpApiSearch extends Component {
   state = {
     result: [],
     location: "alameda",
-
     category: "italian",
     target: 0,
-    times: 8,
+    times: 6,
     duration: 4000,
-    turn: false
+		turn: false,
+		selectedOption: null
   };
-  result = this.state.result;
-  displayResult = (result, ind) => (
-    <p>
-      {result[ind].display_phone}
-      <br />
-      Price: {result[ind].price}
-      <br />
-      Rating: {result[ind].rating}
-    </p>
-  );
+	
+	displayResult = (result, ind) => 
+	(result[ind] ? 
+		(
+		<p>
+			{result[ind].display_phone}
+			<br />
+			Price: {result[ind].price}
+			<br />
+			Rating: {result[ind].rating}
+		</p>
+		) : (
+		<p>
+			<br />
+			<br />
+			<br />
+		</p>
+		)
+	)
+	
   setClicked = () => {
     this.setState({
       target: Math.floor(Math.random() * this.state.result.length),
@@ -43,11 +234,11 @@ class YelpApiSearch extends Component {
   }
 
   searchRestaurants = (location, category) => {
-    console.log("searchRestaurants", location, category);
+    // console.log("searchRestaurants", location, category);
     API.search(location, category)
       .then(res => {
         this.setState({ result: res.data.businesses });
-        console.log(this.state.result);
+        // console.log(this.state.result);
       })
 
       .catch(err => console.log(err));
@@ -58,19 +249,29 @@ class YelpApiSearch extends Component {
     this.setState({
       [name]: value
     });
-  };
+	};
+	
   handleFormSubmit = event => {
-    event.preventDefault();
-    this.searchRestaurants(this.state.location, this.state.category);
+		event.preventDefault();
+		let categories = "";
+		this.state.selectedOption.map(i => categories += i.value + ",");
+		categories = categories.replace(/,$/, "");
+		this.searchRestaurants(this.state.location, categories);
     setTimeout(
       function() {
         this.setClicked();
       }.bind(this),
       3000
     );
-  };
+	};
+	
+	handleChange = (selectedOption) => {
+    this.setState({ selectedOption });
+    // console.log(`Option selected:`, selectedOption);
+  }
 
   render() {
+		const { selectedOption, result, target } = this.state;
     return (
       <div>
         <style jsx="true">
@@ -96,10 +297,10 @@ class YelpApiSearch extends Component {
         <Slot
           className="slot"
           duration={this.state.duration}
-          target={this.state.turn ? this.state.target : 0}
+          target={this.state.turn ? target : 0}
           times={this.state.times}
         >
-          {this.state.result.map((result, i) => (
+          {result.map((result, i) => (
             <div
               key={i}
               className="card text-center slot-item"
@@ -110,15 +311,7 @@ class YelpApiSearch extends Component {
           ))}
         </Slot>
         <br />
-        {this.state.result[1] ? (
-          this.displayResult(this.state.result, this.state.target)
-        ) : (
-          <p>
-            <br />
-            <br />
-            <br />
-          </p>
-        )}
+        {this.displayResult(result, target)}
         <button
           style={{ margin: "1.5em 42em" }}
           className="btn btn-primary blue accent-3"
@@ -126,11 +319,23 @@ class YelpApiSearch extends Component {
         >
           Shuffle Restuarants
         </button>
-
+				<div
+          style={{ width: "50%", margin: "auto" }}
+        >
+        <Select
+				placeholder="Select Categories..."
+        value={selectedOption}
+        onChange={this.handleChange}
+				options={options}
+				menuPlacement="top"
+				isMulti={true}
+				/>
+        </div>
         <Details
           handleInputChange={this.handleInputChange}
           handleFormSubmit={this.handleFormSubmit}
         />
+				
       </div>
     );
   }
--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,17 +1,16 @@
 import axios from "axios";
 
 // const PROXYURL = "https://cors-anywhere.herokuapp.com/";
-// const PROXYURL = "https://crossorigin.me/";
-// const PROXYURL = "http://cors.io/?";
-const PROXYURL = "https://thingproxy.freeboard.io/fetch/";
+// const PROXYURL = "https://thingproxy.freeboard.io/fetch/";
 const BASEURL = "https://api.yelp.com/v3/businesses/search?categories=";
 const BASEURL2 = "&location="
 const APIKEY = "77osbYb8Jaxb5I4iA4bh2qVN7lj4vEVezZe9AbCExk_9t_9F9Cn7DOh9zNYKe9JbKkS4y71PfF21FNyq0w1Z2WF-lzqGbVaPQ3QFjIIj6_WEr753Kqpem_pHUbZ4XHYx";
 
 export default {
     search: function(location, category){
-        console.log(PROXYURL + BASEURL + category + BASEURL2 + location);
-        return axios.get(PROXYURL + BASEURL + category + BASEURL2 + location, {
+				// console.log(PROXYURL + BASEURL + category + BASEURL2 + location);
+				// return axios.get(PROXYURL + BASEURL + category + BASEURL2 + location, {
+        return axios.get(BASEURL + category + BASEURL2 + location, {
             headers: {
                 Authorization: "Bearer " + APIKEY
             }
